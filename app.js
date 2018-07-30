3de91f6b782ef3837aa7697c35dc6e4665658bb2
anonewb <anonewbe@gmail.com>
2018-07-30 19:32:46 +0530

    sw added

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function() {
+		navigator.serviceWorker.register('/sw.js').then(
+			function(registration) {
+				// Registration was successful
+				console.log(
+					'ServiceWorker registration successful with scope: ',
+					registration.scope
+				);
+			},
+			function(err) {
+				// registration failed :(
+				console.log('ServiceWorker registration failed: ', err);
+			}
+		);
+	});
+}
+
 // contains modules, controller modules, LS
 
 /* Steps: eg: after 'clear all' btn is clicked, everything must reset
