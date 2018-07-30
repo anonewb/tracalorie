8957649f2307e8ba3c5ba63278b5c85b496b8e0f
anonewb <anonewbe@gmail.com>
2018-07-30 22:09:47 +0530

    deployed to firebase

--- sw.js
+++ sw.js
@@ -1,17 +1,35 @@
-// importScripts('/cache-polyfill.js');
+importScripts('/cache-polyfill.js');
 
-// self.addEventListener('install', function(e) {
-// 	e.waitUntil(
-// 		caches.open('airhorner').then(function(cache) {
-// 			return cache.addAll([
-// 				'/',
-// 				'/index.html',
-// 				'/index.html?homescreen=1',
-// 				'/?homescreen=1',
-// 				'/styles/main.css',
-// 				'/scripts/main.min.js',
-// 				'/sounds/airhorn.mp3'
-// 			]);
-// 		})
-// 	);
-// });
+self.addEventListener('install', function(e) {
+	e
+		.waitUntil
+		// caches.open('airhorner').then(function(cache) {
+		// 	return cache.addAll([
+		// 		'/',
+		// 		'/index.html',
+		// 		'/index.html?homescreen=1',
+		// 		'/?homescreen=1',
+		// 		'/styles/main.css',
+		// 		'/scripts/main.min.js',
+		// 		'/sounds/airhorn.mp3'
+		// 	]);
+		// })
+		();
+});
+
+self.addEventListener('fetch', function(event) {
+	console.log('Request -->', event.request.url);
+
+	//To tell browser to evaluate the result of event
+	event.respondWith(
+		caches
+			.match(event.request) //To match current request with cached request it
+			.then(function(response) {
+				//If response found return it, else fetch again.
+				return response || fetch(event.request);
+			})
+			.catch(function(error) {
+				console.error('Error: ', error);
+			})
+	);
+});
